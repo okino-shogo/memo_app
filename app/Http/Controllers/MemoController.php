f5a97bf7b427d55002f7e2d99476a6fb09837ea8
okino-shogo <okisho0106@gmail.com>
2025-01-19 12:09:59 +0900

    紹介機能作成

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Memo;
 
 class MemoController extends Controller
+
 {
-    //
+    public function show($id) 
+    {
+        $memo = Memo::find($id);
+        return view('memos.show',['memo'=> $memo]);
+    }
 }
